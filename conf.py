920f9fb6a46bcac8d494a6278ee8b64b5071f4ad
erinecon <erin.conley@canonical.com>
2025-01-28 14:53:41 -0500

    update linkcheck_anchors_ignore_for_url

--- conf.py
+++ conf.py
@@ -213,7 +213,9 @@ linkcheck_ignore = [
 
 # A regex list of URLs where anchors are ignored by 'make linkcheck'
 
-linkcheck_anchors_ignore_for_url = [r"https://github\.com/.*"]
+linkcheck_anchors_ignore_for_url = [r"https://github\.com/.*",
+                                    "https://juju.is/docs/sdk/*",
+                                    ]
 
 
 ########################
